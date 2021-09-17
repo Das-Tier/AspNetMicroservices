6f28d5cdda0c139dc2508b266ce0535758083fa4
vitalii_mohola <vitalijmogola@gmail.com>
2021-09-17 15:01:58 +0300

    add clean architecture

--- src/Services/Discount/Discount.Grpc/Startup.cs
+++ src/Services/Discount/Discount.Grpc/Startup.cs
@@ -13,7 +13,7 @@ namespace Discount.Grpc
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {
+        {            
             services.AddScoped<IDiscountRepository, DiscountRepository>();
             services.AddAutoMapper(typeof(Startup));
             services.AddGrpc();
@@ -22,7 +22,7 @@ namespace Discount.Grpc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            
+           
 
             if (env.IsDevelopment())
             {
